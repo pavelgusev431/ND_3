3042136bda242fbc706ef52187ba659ed03f6e1d
pavelgusev431 <pavel.gusev431@gmail.com>
2025-05-24 23:05:44 +0300

    Basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router";
+
+const App = () => {
+  return (
+    <>
+      <></>
+      <Routes>
+        <Route index element={<></>} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ server/controllers/testController.js
@@ -0,0 +1,8 @@
+import Test from "../models/testModel.js";
+
+const test = () => {
+    const first = new Test();
+    first.test();
+}
+
+export default test;
