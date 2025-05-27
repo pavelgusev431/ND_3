6f28d127a71377946fcbde7c325bf3bb10631b3f
pavelgusev431 <pavel.gusev431@gmail.com>
2025-05-27 12:28:23 +0300

    VITE_API_URL fix

--- client/src/helpers/getURL.js
+++ client/src/helpers/getURL.js
@@ -1,5 +1,5 @@
 const url = (resource) => {
-    return import.meta.env.VITE_API_URL || `http://localhost:5000/${resource}`;
+    return `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/${resource}`;
 };
 
 export default url;
